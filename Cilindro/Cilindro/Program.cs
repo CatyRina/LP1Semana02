ed561eb4bda100f8b0c06a3b12a6741abdb0c8fe
Catarina Fuscaliuc <146738705+CatyRina@users.noreply.github.com>
2024-02-29 22:18:35 +0000

    Add files via upload

---
+++ Cilindro/Cilindro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira a altura (a) do cilindro:");
+        float altura = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o raio (r) do cilindro:");
+        float raio = float.Parse(Console.ReadLine());
+
+        float volume = MathF.PI * MathF.Pow(raio, 2) * altura;
+        float areaSuperficie = 2 * MathF.PI * raio * (raio + altura);
+
+        Console.WriteLine(volume);
+        Console.WriteLine(areaSuperficie);
+    }
+}
+
